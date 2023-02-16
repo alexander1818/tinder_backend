d31a44a31810bc5476bf2c1cc43ff8017c579e5c
Alexandr M <samsungi777s@gmail.com>
2023-02-16 16:23:05 +0200

    added verify/refresh token

---
+++ src/auth/guards/refresh-jwt.guard.ts
@@ -0,0 +1,38 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { UsersService } from 'src/users/users.service';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class RefreshJWTGuard implements CanActivate {
+  constructor(private userService: UsersService) {}
+  async canActivate(
+    context: ExecutionContext,
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    const { refresh_token, username } = request.body;
+
+    if (!refresh_token) {
+      throw new UnauthorizedException('Field refresh_token is required');
+    }
+    if (!username) {
+      throw new UnauthorizedException('Field username is required');
+    }
+
+    const user = await this.userService.findOne(username);
+
+    if (!user) {
+      throw new UnauthorizedException('The user does not exist');
+    }
+
+    return true;
+  }
+}
